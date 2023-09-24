a26a84c573fd605e057b71544ff683f362b36670
email <racbernazgul@gmail.com>
2023-09-24 19:01:51 +0300

    trycatch

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,30 @@
 import { useEffect, useState } from "react";
 import AddTutorial from "../components/AddTutorial";
 import TutorialList from "../components/TutorialList";
-import axios from 'axios'
+import axios from "axios";
 
 const Home = () => {
   const [tutorials, setTutorials] = useState([]);
-  
+
   const BASE_URL = "https://tutorial-api.fullstack.clarusway.com/tutorials/";
 
-  const getTutorials = async() => {
-    const data = await axios(BASE_URL);
-    setTutorials(data.data)
-  }
-  console.log(tutorials)
+  const getTutorials = async () => {
+    try {
+      const { data } = await axios(BASE_URL);
+      setTutorials(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  console.log(tutorials);
 
+  //? maunt asamasında API ye istek atiyoruz
   useEffect(() => {
-    getTutorials()
-  }, [])
+    getTutorials();
+  }, []);
 
   // getTutorials();
- 
+
   return (
     <>
       <AddTutorial />
