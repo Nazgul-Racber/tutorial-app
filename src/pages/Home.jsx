e4d66585397eeffffec62245cae591a765144d5e
email <racbernazgul@gmail.com>
2023-09-24 17:01:46 +0300

    useEffect

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,25 @@
+import { useEffect, useState } from "react";
 import AddTutorial from "../components/AddTutorial";
 import TutorialList from "../components/TutorialList";
 import axios from 'axios'
 
 const Home = () => {
+  const [tutorials, setTutorials] = useState([]);
+  
   const BASE_URL = "https://tutorial-api.fullstack.clarusway.com/tutorials/";
 
   const getTutorials = async() => {
     const data = await axios(BASE_URL);
-    console.log(data.data)
+    setTutorials(data.data)
   }
-  getTutorials()
+  console.log(tutorials)
+
+  useEffect(() => {
+    getTutorials()
+  }, [])
+
+  // getTutorials();
+ 
   return (
     <>
       <AddTutorial />
