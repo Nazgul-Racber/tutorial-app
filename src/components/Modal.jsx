985dda928256931b127525f4584dd9b399ce09be
email <racbernazgul@gmail.com>
2023-09-25 23:47:15 +0300

    Btn: onClick{setDesc..}

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,68 +1,74 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 // EditTutorial.jsx
 const Modal = () => {
-    const [title, setTitle] = useState("");
-    const [description, setDescription] = useState("");
-    return (
-      <div
-        className="modal fade"
-        id="open-Modal"
-        tabIndex={-1}
-        aria-labelledby="exampleModalLabel"
-        aria-hidden="true"
-      >
-        <div className="modal-dialog">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h1 className="modal-title fs-5" id="exampleModalLabel">
-                Edit tutorial
-              </h1>
-              <button
-                type="button"
-                className="btn-close"
-                data-bs-dismiss="modal"
-                aria-label="Close"
-              />
-            </div>
-            <div className="modal-body">
-              <form>
-                <div className="mb-3">
-                  <label htmlFor="title" className="form-label">
-                    Title
-                  </label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="title"
-                    placeholder="Enter your title"
-                    value={title}
-                    onChange={(e) => setTitle(e.target.value)}
-                    required
-                  />
-                </div>
-                <div className="mb-3">
-                  <label htmlFor="desc" className="form-label">
-                    Description
-                  </label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="desc"
-                    placeholder="Enter your Description"
-                    value={description}
-                    onChange={(e) => setDescription(e.target.value)}
-                    required
-                  />
-                </div>
-                <button type="submit" className="btn btn-danger mb-4">
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  return (
+    <div
+      className="modal fade"
+      id="open-Modal"
+      tabIndex={-1}
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h1 className="modal-title text-danger fs-5" id="exampleModalLabel">
+              Edit tutorial
+            </h1>
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+              onClick={() => {
+                setDescription("");
+                setTitle("");
+              }}
+            />
+          </div>
+          <div className="modal-body">
+            <form>
+              <div className="mb-3">
+                <label htmlFor="title" className="form-label">
+                  Title
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="title"
+                  placeholder="Enter your title"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="desc" className="form-label">
+                  Description
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="desc"
+                  placeholder="Enter your Description"
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="text-end">
+                <button type="submit" className="btn btn-danger">
                   Submit
                 </button>
-              </form>
-            </div>
+              </div>
+            </form>
           </div>
         </div>
       </div>
-    );
-}
+    </div>
+  );
+};
 
-export default Modal
+export default Modal;
