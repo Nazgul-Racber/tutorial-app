f26f38906d887d38be2717209e8f428f81aefc7d
email <racbernazgul@gmail.com>
2023-09-25 18:50:27 +0300

    post and get

--- src/components/AddTutorial.jsx
+++ src/components/AddTutorial.jsx
@@ -1,69 +1,70 @@
-import axios from "axios"
-import { useState } from "react"
+import axios from "axios";
+import { useState } from "react";
 
-  const AddTutorial = (getTutorials) => {
-    const [title, setTitle] = useState("");
-    const [description, setDescription] = useState("");
+const AddTutorial = ({ getTutorials }) => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
 
-    const handleSubmit = (e) => {
-      e.preventDefault(); //? sayfanın tekrar refresh olmasını engelledik.
-      const newTutor = { title: title, description: description };
-      console.log(newTutor);
-      postTutorial(newTutor);
-      setTitle("");
-      setDescription("");
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault(); //? sayfanın tekrar refresh olmasını engelledik.
+    const newTutor = { title: title, description: description };
+    console.log(newTutor);
+    postTutorial(newTutor);
 
-    const postTutorial = async (newTutor) => {
-      const BASE_URL =
-        "https://tutorial-api.fullstack.clarusway.com/tutorials/";
-      try {
-        const res = await axios.post(BASE_URL, { newTutor });
-        console.log(res);
-      } catch (error) {
-        console.log(error);
-      }
-      getTutorials()
-    };
-
-    return (
-      <div className="container text-center mt-4">
-        <h1 className="display-6 text-danger">Add Your Tutorial</h1>
-        <form onSubmit={handleSubmit}>
-          <div className="mb-3">
-            <label htmlFor="title" className="form-label">
-              Title
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="title"
-              placeholder="Enter your title"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-              required
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="desc" className="form-label">
-              Description
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="desc"
-              placeholder="Enter your Description"
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-              required
-            />
-          </div>
-          <button type="submit" className="btn btn-danger mb-4">
-            Submit
-          </button>
-        </form>
-      </div>
-    );
+    setTitle("");
+    setDescription("");
   };
 
-export default AddTutorial
+  const postTutorial = async (newTutor) => {
+    const BASE_URL = "https://tutorial-api.fullstack.clarusway.com/tutorials/";
+    try {
+      const res = await axios.post(BASE_URL, newTutor );
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+    //? tüm tutorial'ları iste ve state'i güncelle.
+    getTutorials();
+  };
+
+  return (
+    <div className="container text-center mt-4">
+      <h1 className="display-6 text-danger">Add Your Tutorial</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="title" className="form-label">
+            Title
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="title"
+            placeholder="Enter your title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="desc" className="form-label">
+            Description
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="desc"
+            placeholder="Enter your Description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit" className="btn btn-danger mb-4">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default AddTutorial;
